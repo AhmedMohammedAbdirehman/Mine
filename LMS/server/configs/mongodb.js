1979819d8e9708d40a4d0e6d96be42f33569d984
Ahmed <ahmimo53@gmail.com>
2025-11-25 12:07:06 +0300

    last2

--- LMS/server/configs/mongodb.js
+++ LMS/server/configs/mongodb.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=>{
     mongoose.connection.on('connected', ()=>console.log("database connected"))
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+ await mongoose.connect(process.env.MONGODB_URI, {
+        dbName: 'lms'  // Explicitly set database name
+    })
 }
 
 module.exports = {connectDB}
--- LMS/server/server.js
+++ LMS/server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const {connectDB} = require('./configs/mongodb.js')
  require('dotenv').config()
 const {clerkWebhooks} = require('./controllers/webhooks.js')
+const User = require('./models/User.js') // Require User at the top
 //initialize express
 const app = express()
 
@@ -19,29 +20,6 @@ app.get('/', (req, res)=>res.send("API working"))
 
 
 
-//for test
-app.get('/test-db', async (req, res) => {
-    try {
-        const User = require('./models/User.js')
-        const testUser = await User.create({
-            _id: 'test-user-' + Date.now(),
-            email: 'test@example.com',
-            name: 'Test User',
-            imageUrl: 'https://example.com/image.jpg'
-        })
-        
-        res.json({ 
-            success: true, 
-            message: 'Database and user created successfully!', 
-            user: testUser 
-        })
-    } catch (error) {
-        res.status(500).json({ 
-            success: false, 
-            message: 'Database error teftraul: ' + error.message 
-        })
-    }
-})
 
 
 
